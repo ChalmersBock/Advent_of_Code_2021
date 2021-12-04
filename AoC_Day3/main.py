2bfcd048e32db58129225025bef1152b953cc507
Erik Bock <erikboc@student.chalmers.se>
2021-12-04 22:22:04 +0100

    Solution for AoC day 3 question 2

--- AoC_Day3/main.py
+++ AoC_Day3/main.py
@@ -8,7 +8,6 @@ def parse_input_question_1():
 
     with open(os.path.join(sys.path[0], "data"), "r", encoding='utf-8') as file:
         for line in file.readlines():
-
             for index in range(12):
                 if line[index] == "1":
                     occurrence_list[index] += 1
@@ -31,7 +30,50 @@ def parse_input_question_1():
     print(f'Answer Q1: {gamma*epsilon}')
 
 
+def find_rating(nbr_list, iteration, oxygen):
+    if len(nbr_list) == 0 or len(nbr_list[0]) < iteration:
+        print("Something went wrong")
+        return -1
+
+    counter = 0
+    for nbr in nbr_list:
+        if nbr[iteration] == "1":
+            counter += 1
+        else:
+            counter -= 1
+
+    new_list = []
+    key = "0"
+    if counter < 0:
+        if not oxygen:
+            key = "1"
+    elif oxygen:
+        key = "1"
+
+    for nbr in nbr_list:
+        if nbr[iteration] == key:
+            new_list.append(nbr)
+
+    if len(new_list) == 1:
+        return new_list[0]
+    else:
+        return find_rating(new_list, iteration+1, oxygen)
+
+
+def parse_input_question_2():
+    with open(os.path.join(sys.path[0], "data"), "r", encoding='utf-8') as file:
+        number_list = [x.strip("\n") for x in file.readlines()]
+
+        oxygen_rating_str = find_rating(number_list, 0, True)
+        co2_rating_str = find_rating(number_list, 0, False)
+
+    oxygen_rating = int(oxygen_rating_str, 2)
+    co2_rating = int(co2_rating_str, 2)
+
+    print(f'Answer Q2: {oxygen_rating*co2_rating}')
+
+
 if __name__ == '__main__':
     parse_input_question_1()
-
+    parse_input_question_2()
 
