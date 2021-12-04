27ea0a1376b43ba8071b45e7c3e6f98d33d36bf4
Erik Bock <erikboc@student.chalmers.se>
2021-12-04 19:02:52 +0100

    Finished AoC day 1

--- AoC_Day1/main.py
+++ AoC_Day1/main.py
@@ -3,7 +3,6 @@ import sys
 
 
 def count_increase():
-
     counter = 0
     current_depth = -1
 
@@ -18,22 +17,28 @@ def count_increase():
 
 
 def count_block_increase():
-
     counter = 0
-    current_block_depth = -1
+    queue = []
 
     with open(os.path.join(sys.path[0], "data"), "r", encoding='utf-8') as file:
         for line in file.readlines():
-            depth = int(line)
-            if current_depth != -1 and depth > current_depth:
-                counter += 1
-            current_depth = depth
+            new_value = int(line)
+            if len(queue) < 3:
+                queue.append(new_value)
+            else:
+                old_depth = sum(queue)
+                queue.append(new_value)
+                queue.pop(0)
+                new_depth = sum(queue)
+                if new_depth > old_depth:
+                    counter += 1
 
     return counter
+
 
 if __name__ == '__main__':
     increases = count_increase()
     print(increases)
 
-    increases2 = count_block_increase()
-
+    increases_block = count_block_increase()
+    print(increases_block)
