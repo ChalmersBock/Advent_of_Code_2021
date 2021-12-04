cd7131c1ad227acb09bfd09734bf907dbb9f738e
Erik Bock <erikboc@student.chalmers.se>
2021-12-04 23:57:03 +0100

    Solutions to Day 4 part 2

--- AoC_Day4/main.py
+++ AoC_Day4/main.py
@@ -1,9 +1,10 @@
 import os
 import sys
 
+board_size = 5
 
 class BingoBoard:
-    line_length = 5
+    line_length = board_size
 
     # Takes a list of 25 numbers representing the the bingo board
     def __init__(self, numbers):
@@ -56,7 +57,8 @@ class BingoBoard:
         for row in self.rows:
             print(f'{row}')
 
-def giant_squid_bingo(size):
+
+def get_draw_numbers_and_boards(size):
     with open(os.path.join(sys.path[0], "data"), "r", encoding='utf-8') as file:
         draw_numbers = file.readline().rstrip().split(",")
 
@@ -77,26 +79,63 @@ def giant_squid_bingo(size):
                 new_board_numbers.append(numbers_list[n + i*size])
             bingo_boards.append(BingoBoard(new_board_numbers))
 
-        drawn = []
-        winning_board = None
-        winning_number = -1
+    return (draw_numbers, bingo_boards)
 
-        # Draw numbers until a board gets bingo
-        for number in draw_numbers:
-            drawn.append(int(number))
-            for board in bingo_boards:
-                if board.check_if_bingo(drawn):
-                    winning_board = board
-                    break
 
-            if winning_board:
-                winning_number = int(number)
+def giant_squid_bingo_q1(size):
+    draw_numbers, bingo_boards = get_draw_numbers_and_boards(size)
+    drawn = []
+    winning_board = None
+    winning_number = -1
+
+    # Draw numbers until a board gets bingo
+    for number in draw_numbers:
+        drawn.append(int(number))
+        for board in bingo_boards:
+            if board.check_if_bingo(drawn):
+                winning_board = board
                 break
 
-        print(f'Answer Q1: {winning_number*winning_board.get_non_picked_nbr_sum(drawn)}')
+        if winning_board:
+            winning_number = int(number)
+            break
+
+    print(f'Answer Q1: {winning_number*winning_board.get_non_picked_nbr_sum(drawn)}')
+
+
+def find_surviving_boards(boards, drawn):
+    surviving_boards = []
+    for board in boards:
+        if not board.check_if_bingo(drawn):
+            surviving_boards.append(board)
+    return surviving_boards
+
+
+def giant_squid_bingo_q2(size):
+    draw_numbers, bingo_boards = get_draw_numbers_and_boards(size)
+    drawn = []
+    losing_board = None
+    winning_number = -1
+
+    # Draw numbers until a board gets bingo
+    for number in draw_numbers:
+        drawn.append(int(number))
+        bingo_boards = find_surviving_boards(bingo_boards, drawn)
+
+        # When we have one board left we know that it has lost
+        if len(bingo_boards) == 1:
+            losing_board = bingo_boards[0]
+
+        # The draw that the number of surving boards become 0 is the draw when we should stop
+        if len(bingo_boards) == 0:
+            winning_number = int(number)
+            break
+
+    print(f'Answer Q2: {winning_number * losing_board.get_non_picked_nbr_sum(drawn)}')
 
 
 if __name__ == '__main__':
-    giant_squid_bingo(25)
+    giant_squid_bingo_q1(board_size*board_size)
+    giant_squid_bingo_q2(board_size*board_size)
 
 
