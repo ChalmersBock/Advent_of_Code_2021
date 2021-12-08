c45c876d44332e76044ea8502277dc1b67d4e000
Erik Bock <erikboc@student.chalmers.se>
2021-12-08 18:45:18 +0100

    General cleanup. Removed some classes that could be methods instead

--- AoC_Day8/main.py
+++ AoC_Day8/main.py
@@ -4,97 +4,81 @@ import re
 
 
 def remove_chars(chars, string):
-    new_string = string
     for char in chars:
-        new_string = re.sub(char, "", new_string)
+        string = re.sub(char, "", string)
 
-    return new_string
-
-
-# Only necessary to figure out the positions TopRight, Middle, and BottomLeft to be able to tell all numbers apart
-class Decoder:
-
-    def __init__(self, inp):
-        self.text_codes = ['' for _ in range(10)]
-        self.display = {}
-
-        codes = []
-        numbers_code = inp.strip().split(" ")
-        for code in numbers_code:
-            codes.append(code)
-
-        for code in codes:
-            if len(code) == 2:
-                self.text_codes[1] = code
-            elif len(code) == 4:
-                self.text_codes[4] = code
-            elif len(code) == 7:
-                self.text_codes[8] = code
-
-            if self.text_codes[1] and self.text_codes[4] and self.text_codes[8]:
-                break
-
-        middle_and_top_left = list(remove_chars(self.text_codes[1], self.text_codes[4]))
-        for code in codes:
-            if len(code) == 6:
-                if middle_and_top_left[0] not in code or middle_and_top_left[1] not in code:
-                    self.text_codes[0] = code
-                    break
-
-        if middle_and_top_left[0] in self.text_codes[0]:
-            self.display["Middle"] = middle_and_top_left[1]
-        else:
-            self.display["Middle"] = middle_and_top_left[0]
-
-        for code in codes:
-            if len(code) == 5:
-                if len(remove_chars(code, self.text_codes[4])) == 1 \
-                        and len(remove_chars(code, self.text_codes[1])) != 0:
-                    self.text_codes[5] = code
-                    break
-
-        self.display["TopRight"] = remove_chars(self.text_codes[5], self.text_codes[1])
-
-        self.display["BottomLeft"] = remove_chars(
-            self.display["TopRight"],
-            remove_chars(self.text_codes[5], self.text_codes[8])
-        )
-
-    def get_dict(self):
-        return self.display
+    return string
 
 
-class DisplayNumber:
+# The idea is: Get 1/4 from length -> 0 -> Middle -> 5 -> TopRight -> BottomLeft
+# All other positions and numbers are not necessary to get enough info to tell them apart
+def decode_input(inp):
+    text_codes = ['' for _ in range(10)]
+    display = {}
 
-    def __init__(self, dec, code):
-        self.number_value = -1
+    codes = []
+    numbers_code = inp.strip().split(" ")
+    for code in numbers_code:
+        codes.append(code)
 
+    for code in codes:
         if len(code) == 2:
-            self.number_value = 1
-        elif len(code) == 3:
-            self.number_value = 7
+            text_codes[1] = code
         elif len(code) == 4:
-            self.number_value = 4
-        elif len(code) == 7:
-            self.number_value = 8
-        elif len(code) == 5:
-            if dec.get_dict()["BottomLeft"] in code:
-                self.number_value = 2
-            elif dec.get_dict()["TopRight"] in code:
-                self.number_value = 3
-            else:
-                self.number_value = 5
-        else:
-            if dec.get_dict()["Middle"] in code:
-                if dec.get_dict()["TopRight"] in code:
-                    self.number_value = 9
-                else:
-                    self.number_value = 6
-            else:
-                self.number_value = 0
+            text_codes[4] = code
 
-    def get_number_value(self):
-        return self.number_value
+    first_char, second_char = list(remove_chars(text_codes[1], text_codes[4]))
+    for code in codes:
+        if len(code) == 6 and (first_char not in code or second_char not in code):
+            text_codes[0] = code
+            break
+
+    display["Middle"] = first_char
+    if first_char in text_codes[0]:
+        display["Middle"] = second_char
+
+    for code in codes:
+        if (len(code) == 5
+                and len(remove_chars(code, text_codes[4])) == 1
+                and len(remove_chars(code, text_codes[1])) != 0):
+            text_codes[5] = code
+            break
+
+    display["TopRight"] = remove_chars(text_codes[5], text_codes[4])
+    display["BottomLeft"] = remove_chars(
+        display["TopRight"],
+        remove_chars(text_codes[5], text_codes[0])
+    )
+    return display
+
+
+def get_number_from_code(dec, code):
+
+    if len(code) == 2:
+        number_value = 1
+    elif len(code) == 3:
+        number_value = 7
+    elif len(code) == 4:
+        number_value = 4
+    elif len(code) == 7:
+        number_value = 8
+    elif len(code) == 5:
+        if dec["BottomLeft"] in code:
+            number_value = 2
+        elif dec["TopRight"] in code:
+            number_value = 3
+        else:
+            number_value = 5
+    else:
+        if dec["Middle"] in code:
+            if dec["TopRight"] in code:
+                number_value = 9
+            else:
+                number_value = 6
+        else:
+            number_value = 0
+
+    return number_value
 
 
 def count_unique_digits_in_output():
@@ -116,11 +100,11 @@ def sum_of_outputs():
         for line in file.readlines():
             inp, output = line.split("|")
             numbers_code = output.strip().split(" ")
-            dec = Decoder(inp)
 
             numbers = ""
+            dec = decode_input(inp)
             for code in numbers_code:
-                numbers += str(DisplayNumber(dec, code).get_number_value())
+                numbers += str(get_number_from_code(dec, code))
             output_sum += int(numbers)
 
     print(f'Output Sum: {output_sum}')
