abaa05827591584a819ece29156604051edf0f78
Erik Bock <erikboc@student.chalmers.se>
2021-12-10 10:14:31 +0100

    Small cleanup

--- AoC_Day10/main.py
+++ AoC_Day10/main.py
@@ -8,18 +8,18 @@ match_dict = {
     '<': '>'
 }
 
-value_dict = {
+value_errors = {
     ')': 3,
     ']': 57,
     '}': 1197,
     '>': 25137
 }
 
-value_dict_two = {
-    ')': 1,
-    ']': 2,
-    '}': 3,
-    '>': 4
+value_leftovers = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4
 }
 
 
@@ -45,7 +45,7 @@ def find_corruption():
 
     total_error_sum = 0
     for error in errors:
-        total_error_sum += value_dict[error]
+        total_error_sum += value_errors[error]
     print(f'Total error sum: {total_error_sum}')
 
     return non_corrupt_lines
@@ -55,7 +55,6 @@ def finish_lines(lines):
     score_list = []
     for line in lines:
         stack = []
-        line = line.strip()
         for char in line:
             if char in ['(', '[', '{', '<']:
                 stack.append(char)
@@ -65,15 +64,12 @@ def finish_lines(lines):
         stack.reverse()
         line_sum = 0
         for char in stack:
-            line_sum *= 5
-            line_sum += value_dict_two[match_dict[char]]
+            line_sum = line_sum * 5 + value_leftovers[char]
         score_list.append(line_sum)
-        print(f'Stack: {stack}, score: {line_sum}')
 
     score_list.sort()
     middle_index = int((len(score_list)-1)/2)
     print(f'Final score: {score_list[middle_index]}')
-
 
 
 if __name__ == '__main__':
