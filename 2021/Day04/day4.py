a9944b5d3ece244d695eb15a69b64277653de335
Erik Bock <erikboc@student.chalmers.se>
2021-12-10 23:21:46 +0100

    Small changes to appease linter and some updates to the solution for day 4

--- 2021/Day04/day4.py
+++ 2021/Day04/day4.py
@@ -1,10 +1,9 @@
 import os
 import sys
 
-board_size = 5
 
 class BingoBoard:
-    line_length = board_size
+    line_length = 5  # Hardcoded size of a line
 
     # Takes a list of 25 numbers representing the the bingo board
     def __init__(self, numbers):
@@ -15,14 +14,14 @@ class BingoBoard:
 
         for i in range(self.line_length):
             new_row = []
-            for n in range(self.line_length):
-                new_row.append(numbers[n + i*self.line_length])
+            for j in range(self.line_length):
+                new_row.append(numbers[j + i*self.line_length])
             self.rows.append(new_row)
 
         for i in range(self.line_length):
             new_column = []
-            for n in range(self.line_length):
-                new_column.append(numbers[i + n * self.line_length])
+            for j in range(self.line_length):
+                new_column.append(numbers[i + j * self.line_length])
             self.columns.append(new_column)
 
         new_diagonal = []
@@ -35,107 +34,86 @@ class BingoBoard:
             new_diagonal.append(numbers[(self.line_length - i - 1) + (self.line_length * i)])
         self.diagonals.append(new_diagonal)
 
-    def all_marked(self, line, drawn_nbrs):
-        return all(x in drawn_nbrs for x in line)
+    @staticmethod
+    def all_marked(line, drawn_numbers):
+        return all(x in drawn_numbers for x in line)
 
-    def check_if_bingo(self, drawn_nbrs):
+    def check_if_bingo(self, drawn_numbers):
         for row in self.rows:
-            if self.all_marked(row, drawn_nbrs):
+            if self.all_marked(row, drawn_numbers):
                 return True
         for column in self.columns:
-            if self.all_marked(column, drawn_nbrs):
+            if self.all_marked(column, drawn_numbers):
                 return True
         for diagonal in self.diagonals:
-            if self.all_marked(diagonal, drawn_nbrs):
+            if self.all_marked(diagonal, drawn_numbers):
                 return True
         return False
 
     def get_non_picked_nbr_sum(self, drawn_nbrs):
         return sum(list(set(self.all_numbers) - set(drawn_nbrs)))
 
-    def print_board(self):
-        for row in self.rows:
-            print(f'{row}')
 
-
-def get_draw_numbers_and_boards(size):
+def get_draw_numbers_and_boards():
     with open(os.path.join(sys.path[0], "data"), "r", encoding='utf-8') as file:
         draw_numbers = file.readline().rstrip().split(",")
-
         bingo_boards = []  # To store the board objects
-
         numbers_list = []  # For storing all numbers in the file
+
         for line in file.readlines():
             if line != "\n":
                 line.rstrip()
-                for x in line.split(" "):
-                    if x != "":
-                        numbers_list.append(int(x))
+                for i in line.split(" "):
+                    if i != "":
+                        numbers_list.append(int(i))
 
+        size = 25  # Hardcoded size of boards
         # Split the list into 25 number chunks and create a board from each chunk
         for i in range(int(len(numbers_list)/size)):
             new_board_numbers = []
-            for n in range(size):
-                new_board_numbers.append(numbers_list[n + i*size])
+            for j in range(size):
+                new_board_numbers.append(numbers_list[j + i*size])
             bingo_boards.append(BingoBoard(new_board_numbers))
 
-    return (draw_numbers, bingo_boards)
+    return draw_numbers, bingo_boards
 
 
-def giant_squid_bingo_q1(size):
-    draw_numbers, bingo_boards = get_draw_numbers_and_boards(size)
+def winning_board(draw_numbers, bingo_boards, first_bingo):
     drawn = []
-    winning_board = None
+    selected_board = None
+    boards = bingo_boards
     winning_number = -1
 
-    # Draw numbers until a board gets bingo
     for number in draw_numbers:
         drawn.append(int(number))
-        for board in bingo_boards:
-            if board.check_if_bingo(drawn):
-                winning_board = board
+
+        without_bingo_boards = []
+        for board in boards:
+            if not board.check_if_bingo(drawn):
+                without_bingo_boards.append(board)
+            elif first_bingo:
+                selected_board = board
                 break
 
-        if winning_board:
+        if (first_bingo and selected_board) or len(without_bingo_boards) == 0:
             winning_number = int(number)
             break
 
-    print(f'Answer Q1: {winning_number*winning_board.get_non_picked_nbr_sum(drawn)}')
+        if len(without_bingo_boards) == 1:
+            selected_board = without_bingo_boards[0]
 
+        boards = without_bingo_boards
 
-def find_surviving_boards(boards, drawn):
-    surviving_boards = []
-    for board in boards:
-        if not board.check_if_bingo(drawn):
-            surviving_boards.append(board)
-    return surviving_boards
-
-
-def giant_squid_bingo_q2(size):
-    draw_numbers, bingo_boards = get_draw_numbers_and_boards(size)
-    drawn = []
-    losing_board = None
-    winning_number = -1
-
-    # Draw numbers until a board gets bingo
-    for number in draw_numbers:
-        drawn.append(int(number))
-        bingo_boards = find_surviving_boards(bingo_boards, drawn)
-
-        # When we have one board left we know that it has lost
-        if len(bingo_boards) == 1:
-            losing_board = bingo_boards[0]
-
-        # The draw that the number of surving boards become 0 is the draw when we should stop
-        if len(bingo_boards) == 0:
-            winning_number = int(number)
-            break
-
-    print(f'Answer Q2: {winning_number * losing_board.get_non_picked_nbr_sum(drawn)}')
+    winner_score = winning_number * selected_board.get_non_picked_nbr_sum(drawn)
+    if first_bingo:
+        print(f'Answer Q1 (First bingo score): {winner_score}')
+    else:
+        print(f'Answer Q2 (Last bingo score): {winner_score}')
 
 
 if __name__ == '__main__':
-    giant_squid_bingo_q1(board_size*board_size)
-    giant_squid_bingo_q2(board_size*board_size)
+    _draw_numbers, _bingo_boards = get_draw_numbers_and_boards()
+    winning_board(_draw_numbers, _bingo_boards, True)
+    winning_board(_draw_numbers, _bingo_boards, False)
 
 
