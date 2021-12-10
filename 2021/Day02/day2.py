0f72641a7b4423de713cc1f48c173b9c96ee040f
Erik Bock <erikboc@student.chalmers.se>
2021-12-10 22:36:56 +0100

    Realised q1 and q2 had similar methods, so merged them into one find_position() method

--- 2021/Day02/day2.py
+++ 2021/Day02/day2.py
@@ -2,10 +2,10 @@ import os
 import sys
 
 
-def parse_input_question_1():
-
+def find_position(with_aim):
     horizontal_pos = 0
     depth = 0
+    down_up_value = 0
 
     with open(os.path.join(sys.path[0], "data"), "r", encoding='utf-8') as file:
         for line in file.readlines():
@@ -14,41 +14,23 @@ def parse_input_question_1():
 
             if action == "forward":
                 horizontal_pos += amount
+                if with_aim:
+                    depth += (amount*down_up_value)
             elif action == "down":
-                depth += amount
+                down_up_value += amount
             elif action == "up":
-                depth -= amount
+                down_up_value -= amount
             else:
-                print("Undefined action")
+                print(f'Undefined action: {action}')
 
-    print(f'Answer Q1: {horizontal_pos*depth}')
-
-
-def parse_input_question_2():
-    horizontal_pos = 0
-    depth = 0
-    aim = 0
-
-    with open(os.path.join(sys.path[0], "data"), "r", encoding='utf-8') as file:
-        for line in file.readlines():
-            action, amount_str = line.split(" ")
-            amount = int(amount_str)
-
-            if action == "forward":
-                horizontal_pos += amount
-                depth += (amount*aim)
-            elif action == "down":
-                aim += amount
-            elif action == "up":
-                aim -= amount
-            else:
-                print("Undefined action")
+            if not with_aim:
+                depth = down_up_value
 
     print(f'Answer Q2: {horizontal_pos * depth}')
 
 
 if __name__ == '__main__':
-    parse_input_question_1()
-    parse_input_question_2()
+    find_position(False)
+    find_position(True)
 
 
