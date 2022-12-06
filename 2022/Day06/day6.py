b0ce2ead57463779c2bc4e7d19d31355fb08c2b9
Erik Bock <erikboc@student.chalmers.se>
2022-12-06 20:34:36 +0100

    Small edge case coverage

--- 2022/Day06/day6.py
+++ 2022/Day06/day6.py
@@ -6,6 +6,7 @@ def find_marker_index(marker_length):
         chars = line[i-marker_length:i]
         if len(set(chars)) == len(chars):
             return str(i)
+    return "-1"
 
 
 if __name__ == '__main__':
