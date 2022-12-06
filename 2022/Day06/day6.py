5df9cc80796d41436ac201d2172b53d42b9bdbe8
Erik Bock <erikboc@student.chalmers.se>
2022-12-06 20:29:36 +0100

    Day 6 Solution

---
+++ 2022/Day06/day6.py
@@ -0,0 +1,13 @@
+def find_marker_index(marker_length):
+    with open("data", "r", encoding='utf-8') as file:
+        line = file.readline()
+
+    for i in range(marker_length, len(line)):
+        chars = line[i-marker_length:i]
+        if len(set(chars)) == len(chars):
+            return str(i)
+
+
+if __name__ == '__main__':
+    print("The start-of-packet marker is found after " + find_marker_index(4) + " characters.")
+    print("The message marker is found after " + find_marker_index(14) + " characters.")
